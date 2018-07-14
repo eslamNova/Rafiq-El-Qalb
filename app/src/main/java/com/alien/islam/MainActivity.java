8614611b2e3323486dcde6cfc06bddb26457c283
alien <alien@owl.com>
2018-07-14 15:24:54 +0200

    Added Youtube API

--- app/src/main/java/com/alien/islam/MainActivity.java
+++ app/src/main/java/com/alien/islam/MainActivity.java
@@ -2,12 +2,42 @@ package com.alien.islam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+    Button button1;
+    private YouTubePlayerView youTubePlayerView ;
+    private YouTubePlayer.OnInitializedListener onInitializedListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.viewer);
+        button1 = (Button) findViewById(R.id.button1);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("QWV2rppLaqw");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youTubePlayerView.initialize("AIzaSyC_AJ2MyS2_upuHEkWpSr_6NZjOSMW3u3g",onInitializedListener);
+            }
+        });
     }
 }
